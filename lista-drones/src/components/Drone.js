244233fa0c5948b8c21001c0598bc4074fef9c49
ed <eapesteguiinocente@gmail.com>
2023-12-10 21:07:54 -0500

    lista-drones: Se creo la lista de drones con nombre y precio

--- lista-drones/src/components/Drone.js
+++ lista-drones/src/components/Drone.js
@@ -1,16 +1,12 @@
 import React from 'react';
 
 const Drone = (props) => {
-  const { name, model, price, imageUrl } = props;
+  const { name, price } = props;
 
   return (
-    <div>
-      <img src={imageUrl} alt={name} style={{height:250, width:250}} />
-      <h2>{name}</h2>
-      <p>Modelo: {model}</p>
-      <p>Precio: ${price}</p>
-      <hr></hr>
-    </div>
+    <li>
+      <p>{name} - ${price}</p>
+    </li>
   );
 };
 
