f051c38136b237ae3f96d3c62e2de745a82740a9
ed <eapesteguiinocente@gmail.com>
2023-12-18 17:10:53 -0500

    cambio temporal, mejor estaba antes con aggretsuko :D

--- mi-aplicacion/src/App.js
+++ mi-aplicacion/src/App.js
@@ -5,19 +5,19 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="./aggretsuko2.jpg" style={{borderRadius:80, height:300}} className="App-logo" alt="logo" />
+        <img src="./img1.avif" style={{borderRadius:80, height:300}} alt="logo" />
         <p>
-          Esta imagen es un reflejo de mi experiencia programando en TypeScript, Angular y React.
+          Bienvenido a nuestra página web sobre los vehículos más conocidos en  Perú.
         <br></br>
-        Todo en una MAC y en lo que dura un ciclo.
+        Aquí encontrará los 6 modelos de vehículos más queridos como odiados por el peruano de a pie.
         </p>
         <a
           className="App-link"
-          href="https://react.dev/learn/installation"
+          href="https://gestion.pe/economia/vuelven-plantas-ensamblaje-vehiculos-peru-produce-prepublicara-reglamento-86870-noticia"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Aquí podrás ver cómo instalar REACT
+          Aquí podrás ver más noticias relacionadas
         </a>
       </header>
     </div>
